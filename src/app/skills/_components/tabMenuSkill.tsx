d036988ee2a73ee0cdb1896fa2fe0351158380e5
Roberto Cabral <roberto.cabral@peopleinteractive.com.br>
2024-06-01 21:01:39 -0300

    feat: paginas de skills

---
+++ src/app/skills/_components/tabMenuSkill.tsx
@@ -0,0 +1,86 @@
+
+'use client'
+import { motion } from "framer-motion";
+import Image from 'next/image'
+import logoReact from "../../../../public/devicon--react.svg";
+import logoNext from "../../../../public/icons-nextjs.svg";
+import logoType from "../../../../public/logos--typescript-icon.svg";
+import logoNode from "../../../../public/logos--nodejs-icon-alt.svg";
+import logoExpress from "../../../../public/icons--expressjs.svg";
+import logoCypress from "../../../../public/devicon-plain--cypressio.svg";
+import logoPhp from "../../../../public/icons--php.svg";
+
+
+const skills = [
+  {
+    id: 1,
+    name: "React",
+    image: logoReact.src,
+  },
+  {
+    id: 2,
+    name: "Next.js",
+    image: logoNext.src,
+  },
+  {
+    id: 3,
+    name: "Typescript",
+    image: logoType.src,
+  },
+  {
+    id: 4,
+    name: "Node.js",
+    image: logoNode.src,
+  },
+  {
+    id: 5,
+    name: "Express.js",
+    image: logoExpress.src,
+  },
+  {
+    id: 6,
+    name: "Cypress",
+    image: logoCypress.src,
+  },
+  {
+    id: 7,
+    name: "PHP",
+    image: logoPhp.src,
+  },
+];
+
+
+export default function Skills() {
+  return (
+    <div>
+      <div className="grid w-full grid-cols-2 gap-x-10 md:grid-cols-3">
+        {skills.map((project) => {
+          return (
+            <motion.div
+              whileHover={{
+                y: -8,
+              }}
+              transition={{
+                type: 'spring',
+                bounce: 0.7,
+              }}
+              key={project.id}
+              className="mt-5 text-left"
+            >
+                <Image
+                  src={project.image}
+                  width={30}
+                  height={30}
+                  className="mb-3 rounded-lg border-gray-400 dark:border"
+                  alt={project.name}
+                />
+                <div className="mb-1 text-sm font-medium text-gray-900 dark:text-gray-100">
+                  {project.name}
+                </div>
+            </motion.div>
+          )
+        })}
+      </div>
+    </div>
+  );
+}
--- src/app/skills/page.tsx
+++ src/app/skills/page.tsx
@@ -1,7 +1,14 @@
+import Projects from "@/components/projects";
+import Skills from "./_components/tabMenuSkill";
+
 export default function Page() {
   return (
-    <div className="flex items-center justify-between w-full flex-col  min-h-screen">
-      <h1>skills</h1>
+    <div  className="flex items-center justify-between w-full flex-col pb-8 px-8">
+      <div className="w-full max-w-3xl">
+      <h1 className="font-medium text-gray-900 mb-4 text-lg">Skills</h1>
+
+      <Skills />
+      </div>
     </div>
   );
 }
